acd6ca4d2e78245f440909abaaa8864e7c7b4ed5
Akhil shaji <akhilshaji86@gmail.com>
2022-10-05 10:44:21 +0530

    episode 4

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const title ="Blog title";
+  const obj={a: 10,b: 20};// Objects and booleans cannot be binded in react
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>{Math.random()*100}</p>
+        <p>{JSON.stringify(obj)}</p>
       </div>
     </div>
   );
