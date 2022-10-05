b773a2fc5bff1a9b41c1df72de4bffb2e553488f
Akhil shaji <akhilshaji86@gmail.com>
2022-10-05 10:59:23 +0530

    episode 5

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-
+import Navbar from './Navbar';
+import HomePage from './Home';
 function App() {
-const title ="Blog title";
-  const obj={a: 10,b: 20};// Objects and booleans cannot be binded in react
+  const title="Dojo Blog";
   return (
     <div className="App">
       <div className="content">
-        <h1>{title}</h1>
-        <p>{Math.random()*100}</p>
-        <p>{JSON.stringify(obj)}</p>
+        <Navbar />
+        <HomePage />
       </div>
     </div>
   );
