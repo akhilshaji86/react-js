9c806d023b0162cb34e5151ebb0b56916702c757
Akhil shaji <akhilshaji86@gmail.com>
2022-10-06 16:00:07 +0530

    episode 10

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,6 +1,20 @@
+import {useState} from "react";
+
 const HomePage=() => {
+    const [blogs,setBlogs]=useState([
+        {title: 'Title 1',body: 'body 1',author: 'mario 1',id: 1},
+        {title: 'Title 2',body: 'body 2',author: 'mario 2',id: 2},
+        {title: 'Title 3',body: 'body 3',author: 'mario 3',id: 3}
+    ])
     return (
-        <h1 style={{marginTop: '40px'}} >Home Page</h1>
+        <div className="container">
+            {blogs.map((blog) => (
+                <div className="preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
     );
 }
 
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,20 +1,15 @@
-import {useState} from "react";
 
 const Navbar=() => {
-    const [selectedItem,setselectedItem]=useState();
-    const clicked=(item) => {
-        setselectedItem(item);
-    }
+    
     return (
         <div>
             <nav className="navbar">
                 <h1>Dojo blog</h1>
                 <div className="links">
-                    <a onClick={() => {clicked('Home')}}>Home</a>
-                    <a onClick={() => {clicked('Creat Blog')}}>Creat Blog</a>
+                    <a href="/">Home</a>
+                    <a href="/create">Creat Blog</a>
                 </div>
             </nav>
-            <div>{selectedItem}</div>
         </div>
     );
 }
