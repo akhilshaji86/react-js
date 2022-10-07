96c9f6c914a365fdba0f1446aae320f7410aecfc
Akhil shaji <akhilshaji86@gmail.com>
2022-10-07 14:20:31 +0530

    episode 12

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,13 +3,14 @@ import BlogList from "./Bloglist";
 
 const HomePage=() => {
     const [blogs,setBlogs]=useState([
-        {title: 'Title 1',body: 'body 1',author: 'mario 1',id: 1},
-        {title: 'Title 2',body: 'body 2',author: 'mario 2',id: 2},
-        {title: 'Title 3',body: 'body 3',author: 'mario 3',id: 3}
+        {title: 'Title 1',body: 'body 1',author: 'mario',id: 1},
+        {title: 'Title 2',body: 'body 2',author: 'yoshi',id: 2},
+        {title: 'Title 3',body: 'body 3',author: 'mario',id: 3}
     ])
     return (
         <div className="container">
             <BlogList blogs={blogs} title="All Blogs"></BlogList>
+            <BlogList blogs={blogs.filter((b) => b.author==='mario')} title="Marios's Blogs"></BlogList>
         </div>
     );
 }
